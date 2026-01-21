96efb7e2059c0257872b33c87807061340d8b577
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2026-01-21 19:04:33 +0300

    added initial views.py

--- Kuesuto/main_app/views.py
+++ Kuesuto/main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Plan
 
 def home(request):
-    return HttpResponse('<h1>Hello Planner</h1>')
+    return render(request, 'home.html'),
+
+def about(request):
+    return render(request, 'about.html'),
+
+def plans_index(request):
+    plans = Plan.objects.filter(user=request.user)
+    return render(request, 'plans/index.html', {'plans': plans})
+
+def plans_detail(request, plan_id):
+    plan = Plan.objects.get(id=plan_id)
+    return render(request, 'plans/detail.html', { 'plan':plan })
