4d056eb2a046a2edec925909750da12987864f98
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2026-01-21 18:51:46 +0300

    plan home, index and detail urls

--- Kuesuto/main_app/urls.py
+++ Kuesuto/main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('plans/',views.plans_index, name='plans_index'),
+    path('plans/<int:plan_id>/', views.plan_detail, name='plans_detail')
 ]
 
