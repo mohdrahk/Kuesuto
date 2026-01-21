a6a42fb89bae01d535f612c8f1030a9349d244ba
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2026-01-21 20:31:22 +0300

    static, css, plans folder created, added defs for plans model

--- Kuesuto/main_app/models.py
+++ Kuesuto/main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import date
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Plan(models.Model):
@@ -12,4 +13,11 @@ class Plan(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("plans_detail", kwargs={"plan_id": self.id})
+
 
