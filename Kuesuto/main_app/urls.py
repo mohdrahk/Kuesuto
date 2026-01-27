7b75c55c898db242b9061d6bb6ddfa34ce0ba052
Yaqeen-88 <yaqeen.alabbar@gmail.com>
2026-01-27 13:44:01 +0300

    added the initial CRUD ops + some templates + worked on the PlanCreate

--- Kuesuto/main_app/urls.py
+++ Kuesuto/main_app/urls.py
@@ -5,8 +5,11 @@ from .views import profile
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    # plans CRUD
     path('plans/',views.plans_index, name='plans_index'),
     path('plans/<int:plan_id>/', views.plans_detail, name='plans_detail'),
     path('profile/', views.profile, name='users-profile'),
+    path('plans/create/', views.PlanCreate.as_view(), name='plans_create')
 ]
 
--- Kuesuto/main_app/views.py
+++ Kuesuto/main_app/views.py
@@ -1,7 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Plan
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import transaction
+from .forms import PlanForm, TaskFormSet
+
+class PlanCreate(CreateView):
+    model = Plan
+    form_class = PlanForm
+    template_name = 'main_app/plan_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.POST:
+            context["task_formset"] = TaskFormSet(self.request.POST)
+        else:
+            context["task_formset"] = TaskFormSet()
+        return context
+
+    @transaction.atomic
+    def form_valid(self, form):
+        context = self.get_context_data()
+        task_formset = context["task_formset"]
+
+        if not task_formset.is_valid():
+            return self.form_invalid(form)
+
+        plan = form.save(commit=False)
+        plan.user = self.request.user
+        plan.save()
+
+        task_formset.instance = plan
+        tasks = task_formset.save(commit=False)
+
+        for i, task in enumerate(tasks, start=1):
+            task.plan = plan
+            task.position = i
+            task.save()
+
+        for obj in task_formset.deleted_objects:
+            obj.delete()
+
+        self.object = plan
+        return super().form_valid(form)
+
+
+
+class PlanUpdate(UpdateView):
+    model = Plan
+    fields = ['name', 'duration', 'color']
+
+class PlanDelete(DeleteView):
+    model = Plan
+    success_url = '/plans/'
 
 def home(request):
     return render(request, 'home.html')
