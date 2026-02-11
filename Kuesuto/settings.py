85ed061f3909f80ca4d86e7f0a9e5f4e2b0039ca
“mohdrahk” <mohammedrak@gmail.com>
2026-02-11 18:30:10 +0300

    Prepare Kuesuto for Heroku deployment pt4

--- Kuesuto/settings.py
+++ Kuesuto/settings.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import os
 import dj_database_url
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv()  # Load environment variables from .env file
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
